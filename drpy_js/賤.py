09dce170419139d85d1505553847f2ed68fc0ceb
su147456888444 <159156502+su147456888444@users.noreply.github.com>
2025-09-18 18:32:53 +0800

    Update 賤.py

--- drpy_js/賤.py
+++ drpy_js/賤.py
@@ -25,8 +25,7 @@ class Spider(Spider):
     def destroy(self):
         pass
 
-    #host='https://4icnx7.qyjzlh.com'
-    host='https://ij1men.slsw6.com'
+    host='https://4icnx7.qyjzlh.com'
 
     headers={
         'User-Agent': 'Mozilla/5.0 (Linux; Android 11; M2012K10C Build/RP1A.200720.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/87.0.4280.141 Mobile Safari/537.36;webank/h5face;webank/1.0;netType:NETWORK_WIFI;appVersion:416;packageName:com.jp3.xg3',
@@ -148,3 +147,4 @@ class Spider(Spider):
 
     def liveContent(self, url):
         pass
+
