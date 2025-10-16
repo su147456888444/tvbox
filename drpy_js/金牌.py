24cba93cfdf6bd5a9cd391b73c6c68177e33012a
su147456888444 <159156502+su147456888444@users.noreply.github.com>
2025-10-16 19:41:38 +0800

    Update 金牌.py

--- drpy_js/金牌.py
+++ drpy_js/金牌.py
@@ -384,7 +384,7 @@ class Spider(Spider):
                     'vod_actor': data['vodActor'],
                     'vod_director': data['vodDirector'],
                     'vod_content': data['vodContent'],
-                    'vod_play_from': '老僧酿酒',
+                    'vod_play_from': '书生玩剣ⁱ·*₁＇',
                     'vod_play_url': '#'.join(vod_play_url)
 
                 }
